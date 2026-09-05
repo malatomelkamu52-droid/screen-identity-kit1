2897afe11bc7072b70d1a1b3741f416c83a7f311
malatomelkamu52-droid <malatomelkamu52@gmail.com>
2026-09-06 01:17:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    preview: {
-      host: "0.0.0.0",
-      allowedHosts: [
-        "screen-identity-kit1.onrender.com",
-        ".onrender.com",
-      ],
-    },
-  },
   nitro: {
     preset: "node-server",
   },
